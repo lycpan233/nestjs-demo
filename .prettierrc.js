1fa80b55ac34a6f604c06a2b3c6604ad7d6068c8
流羽 <lycpan@foxmail.com>
2024-10-11 16:23:09 +0800

    style: prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+const config = {
+  printWidth: 120, // 每行代码最多显示的字符数
+  tabWidth: 2, // tab 缩进长度
+  useTabs: false, // 是否使用 tab 进行缩进
+  singleQuote: true, // 是否使用单引号
+  semi: true, // 是否使用分号
+  trailingComma: 'all', // 是否使用尾随逗号
+  arrowParens: 'avoid', // 箭头函数参数是否使用括号
+  bracketSpacing: true, // 是否在括号内使用空格
+  singleAttributePerLine: false, // 是否每个属性独占一行
+};
+
+module.exports = config;
